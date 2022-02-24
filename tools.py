dc62050cbc2d58d7b81005ecdb0d7e9cbfbd6f1c
Hesam Mousavi <hesoyam@Hesams-MacBook-Pro.local>
2022-02-24 17:28:18 +0330

    add assert to new_seed

--- tools.py
+++ tools.py
@@ -16,14 +16,18 @@ def distance(x: np.ndarray, Y: np.ndarray):
     return np.linalg.norm((x[:, np.newaxis, :]-Y), axis=-1)
 
 
-def new_seed(X, L, dist):
+def new_seed(X, L, probability):
     """
     Parameters:
        X: nxd
-       dist: nx1
+       probability: nx1
        L: number of new seed
 
     Returns:
         z: ndarray Lxd contain L new seed(s)
     """
-    return X[np.random.choice(range(len(dist)), L, p=dist), :]
+    assert len(X.shape) == 2, 'X should be 2-D array'
+    assert len(probability.shape) == 2 and probability.shape[1] == 1,\
+        'probability should be 2-D array nx1'
+    assert probability.shape[0] == X.shape[0]
+    return X[np.random.choice(range(len(probability)), L, p=probability), :]
