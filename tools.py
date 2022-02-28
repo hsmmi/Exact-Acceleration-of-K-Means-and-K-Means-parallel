71897fee2c4061dd0ca3ab020a6cfa3306e16917
mhdi <sharesomethingforfun@gmail.com>
2022-02-28 21:59:29 +0330

    delete unnecessary function and add comment

--- tools.py
+++ tools.py
@@ -1,11 +1,13 @@
 import numpy as np
 from functools import wraps
 from time import time
-import pickledb
 from scipy.spatial.distance import cdist
 
 
 def tlogger(f):
+    """Decorator for Store total computated distance
+    Return associated function
+    """
     def wrapped(*args, **kwargs):
         if type(args[0]) == tuple:
             wrapped.sum += 1
@@ -18,42 +20,9 @@ def tlogger(f):
             y = args[1].shape[0]
             wrapped.sum += x * y
         return f(*args, **kwargs)
-
+    # Define variable for store total computated distance
     wrapped.sum = 0
     return wrapped
-
-
-def logger(f):
-    #  computations logger
-    @wraps(f)
-    def wrap(*args, **kw):
-        x = args[0].shape[0]
-        y = args[1].shape[0]
-        num_of_computations = x * y
-        db = pickledb.load("log.db", False)
-        db.set("c_sum", db.get("c_sum") + num_of_computations)
-        c = db.get("computations")
-        db.set("computations", [*c, num_of_computations])
-        db.dump()
-        return f(*args, **kw)
-
-    return wrap
-
-
-def db_init():
-    db = pickledb.load("log.db", False)
-    db.set("computations", [])
-    db.set("c_sum", 0)
-    db.dump()
-    return True
-
-
-def get_log():
-    try:
-        db = pickledb.load("log.db", False)
-        return db.get("computations"), db.get("c_sum")
-    except Exception as e:
-        print(e)
 
 
 @tlogger
@@ -77,28 +46,9 @@ def distance(X: np.ndarray, Y: np.ndarray):
     return cdist(X, Y)
 
 
-def distance2(X: np.ndarray, Y: np.ndarray):
-    """Using numpy to find distance of each pair (x, y)
-    Parameters:
-        X: nxd data(s)
-        Y: mxd point(s)
-
-    Returns:
-        d: nxm ndarray contain distance of data to each point
-    """
-    if len(X.shape) == 1:
-        X = X.reshape((1, -1))
-    if len(Y.shape) == 1:
-        Y = Y.reshape((1, -1))
-    assert len(X.shape) == 2, "X should be nxd ndarray"
-    assert (
-        len(Y.shape) == 2 and Y.shape[1] == X.shape[1]
-    ), "y should be in X space"
-    return np.linalg.norm((X[:, np.newaxis, :] - Y), axis=-1)
-
-
 def new_seed(X, L, probability):
-    """
+    """Select L new seed(s) with our probability from X
+
     Parameters:
        X: nxd
        probability: nx1
@@ -119,6 +69,9 @@ def new_seed(X, L, probability):
 
 
 def execution_time(f):
+    """Decorator for compute runtime of funtion
+    Return associated function
+    """
     @wraps(f)
     def wrap(*args, **kw):
         ts = time()
